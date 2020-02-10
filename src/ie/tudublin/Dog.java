a171278682cbb85cd5f303223e6f82f6d9281a3c
unknown <patryk2710dd@gmail.com>
2020-02-10 11:59:35 +0000

    Dogs

---
+++ src/ie/tudublin/Dog.java
@@ -0,0 +1,11 @@
+package ie.tudublin;
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+    
+    public void speak() {
+        System.out.println("Wuf!");
+    }
+}
